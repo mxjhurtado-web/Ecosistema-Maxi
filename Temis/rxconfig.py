39e399543ebc5637857e2f94f26d2605851d67d1
Jose Antonio Hurtado Palestina <mxjhurtado@maxillc.com>
2026-08-27 18:47:41 -0600

    fix: configura RadixThemesPlugin de forma explicita en rxconfig.py y show_built_with_reflex=False

--- Temis/rxconfig.py
+++ Temis/rxconfig.py
@@ -1,6 +1,7 @@
 import os
 import reflex as rx
 from reflex_base.plugins.sitemap import SitemapPlugin
+from reflex_components_radix.plugin import RadixThemesPlugin
 
 db_url = os.getenv("DATABASE_URL", "sqlite:///temis.db")
 if db_url.startswith("postgres://"):
@@ -10,5 +11,7 @@ config = rx.Config(
     app_name="temis_web",
     db_url=db_url,
     telemetry_enabled=False,
+    show_built_with_reflex=False,
+    plugins=[RadixThemesPlugin()],
     disable_plugins=[SitemapPlugin],
 )
